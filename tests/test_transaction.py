fd4f6a3fc15511c5ab83f3df929dd554d17a0ec9
Nicole Vavrova <vavrova.n@gmail.com>
2017-10-30 14:43:20 +0000

    implement first test

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -8,7 +8,16 @@ def test_changes_applied_on_successful_exit():
     with testing.postgresql.Postgresql() as postgresql:
         cxn = psycopg2.connect(**postgresql.dsn())
         with Transaction(cxn):
-            pass
+            cur = cxn.cursor()
+            create_tmp_table(cur)
+            cur.execute("INSERT INTO tmp_table VALUES ('hello')")
+        cur.execute("SELECT * FROM tmp_table")
+        rows = cur.fetchall()
+        assert len(rows) == 1
+
+
+def create_tmp_table(cur):
+    cur.execute("CREATE TEMPORARY TABLE tmp_table(Id VARCHAR(80)) ON COMMIT PRESERVE ROWS")
 
 
 def test_changes_discarded_on_exception():
