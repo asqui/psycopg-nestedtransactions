f035d89119bc7caf7bcb198038f3edfdd5016fa2
Nicole Vavrova <vavrova.n@gmail.com>
2017-10-30 11:57:30 +0000

    move requirements into setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='psycopg-nestedtransactions',
+      version='1.0',
+      description='Database transaction manager for psycopg2 database connections with seamless support for nested transactions.',
+      url='https://github.com/asqui/psycopg-nestedtransactions',
+      packages=[],
+      install_requires=['psycopg2'],
+      extras_require=dict(
+          test=['pytest', 'testing.postgresql']
+      )
+      )
