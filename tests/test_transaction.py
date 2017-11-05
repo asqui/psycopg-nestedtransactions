948491bc6644489a54178e97fb2a6009124bb191
Nicole Vavrova <vavrova.n@gmail.com>
2017-11-05 23:38:38 +0000

    adding basic transaction functionality and related tests

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,48 +1,106 @@
-import psycopg2
+import psycopg2, psycopg2.extensions
+import pytest
 import testing.postgresql
 
 from nestedtransactions.transaction import Transaction
 
 
-def test_changes_applied_on_successful_exit():
-    with testing.postgresql.Postgresql() as postgresql:
-        cxn = psycopg2.connect(**postgresql.dsn())
+@pytest.fixture(scope='module', name='cxn')
+def create_connection_and_db_table(request):
+    postgresql = testing.postgresql.Postgresql()
+
+    def fin():
+        postgresql.stop()
+
+    request.addfinalizer(fin)
+
+    cxn = psycopg2.connect(**postgresql.dsn())
+    cur = cxn.cursor()
+    cur.execute("CREATE TABLE tmp_table(Id VARCHAR(80))")
+    return cxn
+
+
+def test_no_open_transactions_on_successful_exit(cxn):
+    # commit to make this test independent of the order
+    #   - should we solve this in a better way? do we even care about the first not in transaction assert?
+    cxn.commit()
+
+    assert_not_in_transaction(cxn)
+    with Transaction(cxn):
+        cxn.cursor().execute('SELECT 1')
+        assert_in_transaction(cxn)
+    assert_not_in_transaction(cxn)
+
+
+def test_no_open_transactions_on_exception(cxn):
+    # commit to make this test independent of the order
+    #   - should we solve this in a better way? do we even care about the first not in transaction assert?
+    cxn.commit()
+
+    assert_not_in_transaction(cxn)
+    try:
         with Transaction(cxn):
-            cur = cxn.cursor()
-            create_tmp_table(cur)
-            cur.execute("INSERT INTO tmp_table VALUES ('hello')")
-        cur.execute("SELECT * FROM tmp_table")
-        rows = cur.fetchall()
-        assert len(rows) == 1
+            cxn.cursor().execute('SELECT 1')
+            assert_in_transaction(cxn)
+            raise ExpectedException('This rolls back the transaction')
+    except ExpectedException:
+        pass
+    assert_not_in_transaction(cxn)
 
 
-def create_tmp_table(cur):
-    cur.execute("CREATE TEMPORARY TABLE tmp_table(Id VARCHAR(80)) ON COMMIT PRESERVE ROWS")
+def test_changes_applied_on_successful_exit(cxn):
+    cur = cxn.cursor()
+    with Transaction(cxn):
+        cur.execute("INSERT INTO tmp_table VALUES ('test_changes_applied_on_successful_exit')")
+    cur.execute("SELECT * FROM tmp_table WHERE Id = 'test_changes_applied_on_successful_exit'")
+    rows = cur.fetchall()
+    assert len(rows) == 1
 
 
-def test_changes_discarded_on_exception():
+def test_changes_discarded_on_exception(cxn):
+    cur = cxn.cursor()
+    try:
+        with Transaction(cxn):
+            cur.execute("INSERT INTO tmp_table VALUES ('test_changes_discarded_on_exception')")
+            raise ExpectedException('This discards the insert')
+    except ExpectedException:
+        pass
+    cur.execute("SELECT * FROM tmp_table WHERE Id = 'test_changes_discarded_on_exception'")
+    rows = cur.fetchall()
+    assert len(rows) == 0
+
+
+def test_forced_discard_changes_discarded_on_successful_exit(cxn):
     pass
 
 
-def test_forced_discard_changes_discarded_on_successful_exit():
+def test_forced_discard_changes_discarded_on_exception(cxn):
     pass
 
 
-def test_forced_discard_changes_discarded_on_exception():
+def test_inner_and_outer_changes_persisted_on_successful_exit(cxn):
     pass
 
 
-def test_inner_and_outer_changes_persisted_on_successful_exit():
+def test_inner_and_outer_changes_discarded_on_outer_exception(cxn):
     pass
 
 
-def test_inner_and_outer_changes_discarded_on_outer_exception():
+def test_inner_and_outer_changes_discarded_on_unhandled_inner_exception(cxn):
     pass
 
 
-def test_inner_and_outer_changes_discarded_on_unhandled_inner_exception():
+def test_inner_changes_discarded_on_exception_but_outer_changes_persisted_on_successful_exit(cxn):
     pass
 
 
-def test_inner_changes_discarded_on_exception_but_outer_changes_persisted_on_successful_exit():
+def assert_in_transaction(cxn):
+    assert cxn.status == psycopg2.extensions.STATUS_IN_TRANSACTION
+
+
+def assert_not_in_transaction(cxn):
+    assert cxn.status == psycopg2.extensions.STATUS_READY
+
+
+class ExpectedException(Exception):
     pass
